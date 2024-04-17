dee4c79dd0e7bf11c97c1ce5efdd04f88db17dc3
alijanovashukrona <146185050+alijanovashukrona@users.noreply.github.com>
2024-04-17 16:40:49 +0500

    ./

--- newsW/newsW/settings.py
+++ newsW/newsW/settings.py
@@ -128,6 +128,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
+
+EMAIL_BACKEND ='django.core.mail.backends.console.EmailBackend'
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
